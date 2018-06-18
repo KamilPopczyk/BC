9322631c0dbb4dc24d8f6da8f5eec2b45aa78725
Kamil <kam.popczyk@gmail.com>
2018-06-18 20:34:11 +0200

    Localhost quite good

--- buttonz-counter.py
+++ buttonz-counter.py
@@ -1,5 +1,7 @@
 from html.parser import HTMLParser
 import urllib.request
+from contextlib import closing
+import socket
 
 
 class ButtonCounter(HTMLParser):
@@ -37,16 +39,22 @@ class WebsiteCounter:
     def load_website(self, url):
         """Function responsible for download website's html"""
         if url is 'localhost':
+            locahost_url = 'http://localhost:'     # example: http://localhost:8080/'
+            used_port = 0
+            for port in range(8000, 8081):            # Let's find used port
+                with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+                    res = sock.connect_ex(('localhost', port))
+                    if res == 0:
+                        used_port = port
             try:
-                with urllib.request.urlopen('http://localhost:8080/') as response:
+                with urllib.request.urlopen(locahost_url + str(port) + '/') as response:
                     self.html_response = str(response.read())
             except urllib.request.HTTPError:
                 print("Can't download website.")
                 return False
             except:
-                print("Something wrong with website.")
-                return False
-
+                print("Unused port: " + str(port))
+        # end if localhost
         else:
             if not ('https://' or 'http://') in url:
                 url = 'https://' + url
@@ -59,6 +67,7 @@ class WebsiteCounter:
             except:
                 print("Something wrong with website.")
                 return False
+
 
     def count_buttons(self):
         html_parser = ButtonCounter()
