da773dc04622e7895fb1ee500d6a06c5f31ffca0
Kamil <kam.popczyk@gmail.com>
2018-06-18 19:59:25 +0200

    Button counter end :)

--- buttonz-counter.py
+++ buttonz-counter.py
@@ -10,15 +10,24 @@ class ButtonCounter(HTMLParser):
         self.button_sum = 0
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'div':
+        """Search buttons"""
+        if tag == 'button':     # html tag <button>
             self.button_sum = self.button_sum + 1
-            print(tag)
-            print("Encountered a start tag:", tag)
+
+        if tag == 'input':      # html tag <input> with type = submit,reset,button
             for attr in attrs:
-                print("     attr:", attr)
+                if attr[0] == 'type':
+                    if attr[1] == 'submit' or attr[1] == 'reset' or attr[1] == 'button':
+                        self.button_sum = self.button_sum + 1
+
+        if tag == 'a':          # html tag <a> with class which contains keywords: btn, button
+            for attr in attrs:
+                if attr[0] == 'class':
+                    if attr[1].lower().find('btt') >= 0 or attr[1].lower().find('button') >= 0:
+                        self.button_sum = self.button_sum + 1
 
 
-class WebsiteCounter(HTMLParser):
+class WebsiteCounter:
     """Class website counter"""
     def __init__(self):
         self.button_sum = 0
@@ -26,17 +35,22 @@ class WebsiteCounter(HTMLParser):
 
     def load_website(self, url):
         """Function responsible for download website's html"""
-        if not ('https://' or 'http://') in url:
-            url = 'https://' + url
-        try:
-            with urllib.request.urlopen(url) as response:
-                self.html_response = str(response.read())
-        except urllib.request.HTTPError:
-            print("Can't download website.")
-            return False
-        except:
-            print("Something wrong with website.")
-            return False
+        if url is 'localhost':
+            localhost = urllib.urlopen("http://localhost:8280/")
+            print(localhost.read(100))
+
+        else:
+            if not ('https://' or 'http://') in url:
+                url = 'https://' + url
+            try:
+                with urllib.request.urlopen(url) as response:
+                    self.html_response = str(response.read())
+            except urllib.request.HTTPError:
+                print("Can't download website.")
+                return False
+            except:
+                print("Something wrong with website.")
+                return False
 
     def count_buttons(self):
         html_parser = ButtonCounter()
@@ -45,7 +59,7 @@ class WebsiteCounter(HTMLParser):
 
 def main():
     cou = WebsiteCounter()
-    cou.load_website('www.boredbutton.com')
+    cou.load_website('www.boredbutton.com') # poprawic localhost ! www.boredbutton.com
     cou.count_buttons()
 
 
